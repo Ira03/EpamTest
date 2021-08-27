257c69e8421918f129be2dfb9c8552a81ad22682
Iryna Yakubenia <i.yakubenia@gmail.com>
2021-08-27 18:23:36 +0300

    feat: header

---
+++ task/src/components/Header/header.js
@@ -0,0 +1,10 @@
+import './header.scss';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1>Смартфоны</h1>
+      <span className="header__text">898</span>
+    </header>
+  );
+};
